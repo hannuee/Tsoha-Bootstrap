9671c007b82141cbca47e5caa3cb2030feefee98
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2018-02-02 13:55:24 +0200

    bugikorjaus

--- app/controllers/front_controller.php
+++ app/controllers/front_controller.php
@@ -3,7 +3,7 @@
 class FrontController extends BaseController{
     
     public static function index(){
-        $oluterat = Oluterat::allAvailableWithMargin(400);
+        $oluterat = Olutera::allAvailableWithMargin(400);
         View::make('frontpage.html', array('oluterat' => $oluterat));
     }
     
